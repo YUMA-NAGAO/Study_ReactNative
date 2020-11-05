4d782cb5a3c5acdae1f4e672a6e9da34320811c2
YUMA-NAGAO <55423883+YUMA-NAGAO@users.noreply.github.com>
2020-11-05 14:37:13 +0900

    [edit]15章の途中のコード

---
+++ Item雛形.tsx
@@ -0,0 +1,14 @@
+import react from "react";
+import { View, StyleSheet } from "react-native";
+
+type Props = {
+
+};
+
+export const ShopReviewItem: react.FC<Props> = ({ }: Props) => {
+    return <View style={styles.container} ></View>;
+}
+
+const styles = StyleSheet.create({
+    container: {}
+});
