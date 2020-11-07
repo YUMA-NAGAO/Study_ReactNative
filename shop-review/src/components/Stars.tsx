d5aaac6db0ea82a2fba373e0c3a2c8b5cc9aeb61
YUMA-NAGAO <55423883+YUMA-NAGAO@users.noreply.github.com>
2020-11-07 15:51:11 +0900

    [edit]13:09までを進めた

--- shop-review/src/components/Stars.tsx
+++ shop-review/src/components/Stars.tsx
@@ -1,18 +1,29 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet ,Text} from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 
 
 type Props = {
     // ?をつけるときは、任意の値を入れる事ができるようになる
-    score: number;
-    starSize?: number;
-    textSize?: number;
+  score: number;
+  starSize?: number;
+  textSize?: number;
 };
 
-export const Stars: React.FC<Props> = ({ score,starSize=16,textSize=14}: Props) => {
-    return <View style={styles.container} >
-    </View>;
+export const Stars: React.FC<Props> = ({
+    score, starSize = 16, textSize = 14,
+    
+}:Props) => {
+    return (<View style={styles.container} >
+        <FontAwesome name="star" style={[styles.star, { fontSize: starSize }]} />
+        <FontAwesome name="star" style={[styles.star, { fontSize: starSize }]} />
+        <FontAwesome name="star" style={[styles.star, { fontSize: starSize }]} />
+        <FontAwesome name="star" style={[styles.star, { fontSize: starSize }]} />
+        <FontAwesome name="star" style={[styles.star, { fontSize: starSize }]} />
+        <Text style={[styles.scoreText, { fontSize: textSize }]}>{score}</Text>
+        
+    </View>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -20,5 +31,9 @@ const styles = StyleSheet.create({
     star: {
         color: "#000",
         marginRight:4,
-    }
+    },
+    scoreText: {
+        color: "#000",
+        fonwtWeight:"bold",
+    },
 }); 
